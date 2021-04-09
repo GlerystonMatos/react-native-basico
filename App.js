6e00a0de4259c71c43c1cde113082a3d4c640ed6
Gleryston Matos <glerystonmatos@yahoo.com.br>
2021-04-09 07:09:34 -0300

    Implementando StatusBar e Header

--- App.js
+++ App.js
@@ -1,16 +1,15 @@
 import * as React from "react";
 import { View, Text } from "react-native";
+import StatusBar from './src/components/StatusBar';
+import Header from './src/components/Header';
 
 export default function App() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Universal React with Expo</Text>
+    <View>
+      <StatusBar corFundo='blue' />
+      <Header titulo='Menu Principal' />
+      <Text>Hello Word</Text>
+      <Text>Gleryston Matos</Text>
     </View>
   );
 }
